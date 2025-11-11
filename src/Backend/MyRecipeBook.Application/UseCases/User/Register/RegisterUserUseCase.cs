43db330d063dfa2516ac8dd0adfe4c43b2f66fcd
Matheus Rocha <matheus.alvesrocha@hotmail.com>
2025-11-11 15:41:25 -0300

    feat: adding usecase to register user

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,74 @@
+﻿using AutoMapper;
+using MyRecipeBook.Application.Services.AutoMapper;
+using MyRecipeBook.Application.Services.Cryptography;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase : IRegisterUserUseCase
+    {
+        private readonly IUserWriteOnlyRepository _writeOnlyRepository;
+        private readonly IUserReadOnlyRepository _readOnlyRepository;
+        private readonly IUnityOfWork _unityOfWork;
+        private readonly PasswordEncrypter _passwordEncrypter;
+        private readonly IMapper _mapper;
+
+        public RegisterUserUseCase(
+            IUserWriteOnlyRepository writeOnlyRepository,
+            IUserReadOnlyRepository readOnlyRepository,
+            IUnityOfWork unityOfWork,
+            PasswordEncrypter passwordEncrypter,
+            IMapper mapper)
+        {
+            _writeOnlyRepository = writeOnlyRepository;
+            _readOnlyRepository = readOnlyRepository;
+            _passwordEncrypter = passwordEncrypter;
+            _mapper = mapper;
+            _unityOfWork = unityOfWork;
+
+        }
+
+        public async Task<ResponseRegisterUserJson> Execute(RequestRegisterUserJson request)
+        {
+            await Validate(request);
+
+            var user = _mapper.Map<Domain.Entities.User>(request);
+            user.Password = _passwordEncrypter.Encrypt(request.Password);
+
+            await _writeOnlyRepository.Add(user);
+
+            await _unityOfWork.Commit();
+
+            return new ResponseRegisterUserJson
+            {
+                Name = user.Name,
+            };
+        }
+        public async Task Validate(RequestRegisterUserJson request)
+        {
+            var validator = new RegisterUserValidator();
+
+            var result = validator.Validate(request);
+
+            var emailExist = await _readOnlyRepository.ExistActiveUserWithEmail(request.Email);
+
+            if (emailExist)
+            {
+                result.Errors.Add(new FluentValidation.Results.ValidationFailure(string.Empty, ResourceMessagesException.EMAIL_ALREADY_REGISTERED));
+            }
+
+            if (result.IsValid == false)
+            {
+                var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+
+                throw new ErrorOnValidationException(errorMessages);
+            }
+        }
+    }
+
+}
