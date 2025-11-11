fb3fae1544f36b0e349a06384ccb18bb296717f4
Matheus Rocha <matheus.alvesrocha@hotmail.com>
2025-11-11 15:39:56 -0300

    feat: adding repositories on infrastructure

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/UnityOfWork.cs
@@ -0,0 +1,18 @@
+﻿using MyRecipeBook.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Infrastructure.DataAccess
+{
+    public class UnityOfWork : IUnityOfWork
+    {
+        private readonly MyRecipeBookDbContext _dbContext;
+
+        public UnityOfWork(MyRecipeBookDbContext dbContext) => _dbContext = dbContext;
+
+        public async Task Commit() => await _dbContext.SaveChangesAsync();
+    }
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/DependenceInjectionExtension.cs
@@ -0,0 +1,61 @@
+﻿using FluentMigrator.Runner;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.User;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repository;
+using MyRecipeBook.Infrastructure.Extensions;
+using System.Reflection;
+
+namespace MyRecipeBook.Infrastructure
+{
+    public static class DependenceInjectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            AddRepositories(services);
+
+            if (configuration.IsUnitTestEnviroment())
+            {
+                return;
+            }
+
+            AddDbContext(services, configuration);
+            AddFluentMigrator(services, configuration);
+        }
+
+        private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.ConnectionString();
+            var serverVersion = new MySqlServerVersion(new Version(8, 4, 4));
+
+            services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions =>
+            {
+                dbContextOptions.UseMySql(connectionString, serverVersion);
+            });
+        }
+
+        private static void AddRepositories(IServiceCollection services)
+        {
+            services.AddScoped<IUnityOfWork, UnityOfWork>();
+
+            services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+            services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        }
+
+        private static void AddFluentMigrator(IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.ConnectionString();
+
+            services.AddFluentMigratorCore().ConfigureRunner(options =>
+            {
+                options
+                .AddMySql5()
+                .WithGlobalConnectionString(connectionString)
+                .ScanIn(Assembly.Load("MyRecipeBook.Infrastructure")).For.All();
+            });
+        }
+    }
+}
