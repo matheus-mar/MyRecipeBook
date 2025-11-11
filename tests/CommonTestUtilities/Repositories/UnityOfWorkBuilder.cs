63b6949055286d3cd23e1b7e136b82744ca60f24
Matheus Rocha <matheus.alvesrocha@hotmail.com>
2025-11-11 15:47:42 -0300

    tests: adding unit test for usecase

---
+++ tests/CommonTestUtilities/Repositories/UnityOfWorkBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Repositories;
+
+namespace CommonTestUtilities.Repositories
+{
+    public class UnityOfWorkBuilder
+    {
+        public static IUnityOfWork Build()
+        {
+            var mock = new Mock<IUnityOfWork>();
+
+            return mock.Object;
+        }
+    }
+}
---
+++ tests/UseCases.Test/User/Register/RegisterUserUseCaseTest.cs
@@ -0,0 +1,77 @@
+﻿using CommonTestUtilities.Cryptography;
+using CommonTestUtilities.Mapper;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Exceptions;
+using MyRecipeBook.Exceptions.ExceptionsBase;
+using Shouldly;
+
+namespace UseCases.Test.User.Register
+{
+    public class RegisterUserUseCaseTest
+    {
+        [Fact]
+        public async Task Success()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var useCase = CreateUseCase();
+
+            var result = await useCase.Execute(request);
+
+            result.ShouldNotBeNull();
+            result.Name.ShouldBe(request.Name);
+        }
+
+        [Fact]
+        public async Task Error_Email_Already_Registered()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+
+            var useCase = CreateUseCase(request.Email);
+
+            Func<Task> act = async () => await useCase.Execute(request);
+
+            var exception = await Should.ThrowAsync<ErrorOnValidationException>(act);
+            exception.ErrorMessages.ShouldSatisfyAllConditions(
+                errorMessages => errorMessages.Count.ShouldBe(1),
+                errorMessages => errorMessages.ShouldContain(ResourceMessagesException.EMAIL_ALREADY_REGISTERED)
+            );
+        }
+
+        [Fact]
+        public async Task Error_Name_Empty()
+        {
+            var request = RequestRegisterUserJsonBuilder.Build();
+            request.Name = string.Empty;
+            
+            var useCase = CreateUseCase();
+
+            Func<Task> act = async () => await useCase.Execute(request);
+
+            var exception = await Should.ThrowAsync<ErrorOnValidationException>(act);
+            exception.ErrorMessages.ShouldSatisfyAllConditions(
+                errorMessages => errorMessages.Count.ShouldBe(1),
+                errorMessages => errorMessages.ShouldContain(ResourceMessagesException.NAME_EMPTY)
+            );
+        }
+
+        private RegisterUserUseCase CreateUseCase(string? email = null)
+        {
+            var mapper = MapperBuilder.Build();
+            var passwordEncrypter = PasswordEncrypterBuilder.Build();
+            var writeRepository = UserWriteOnlyRepositoryBuilder.Build();
+            var unityOfWork = UnityOfWorkBuilder.Build();
+            var readRepositoryBuilder = new UserReadOnlyRepositoryBuilder();
+
+            if (string.IsNullOrEmpty(email) == false)
+            {
+                readRepositoryBuilder.ExistActiveUserWithEmail(email);
+            }
+
+                return new RegisterUserUseCase(writeRepository, readRepositoryBuilder.Build(), unityOfWork, passwordEncrypter, mapper);
+
+        }
+    }
+}
