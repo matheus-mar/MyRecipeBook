360d5e2a1a889fe358b48decdfc87699c5b0c77d
Matheus Rocha <matheus.alvesrocha@hotmail.com>
2025-11-22 22:44:35 -0300

    tests: adding integration test for login

---
+++ tests/WebApi.Test/Login/DoLogin/DoLoginTest.cs
@@ -0,0 +1,86 @@
+﻿using CommonTestUtilities.Requests;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Exceptions;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApi.Test.InlineData;
+
+namespace WebApi.Test.Login.DoLogin
+{
+    public class DoLoginTest : MyRecipeBookClassFixture
+    {
+        private readonly string method = "login";
+
+        private readonly string _email;
+
+        private readonly string _password;
+
+        private readonly string _name;
+
+        public DoLoginTest(CustomWebApplicationFactory factory) : base(factory)
+        {
+            _email = factory.GetEmail();
+            _password = factory.GetPassword();
+            _name = factory.GetName();
+        }
+
+
+        [Fact]
+        public async Task Success()
+        {
+            var request = new RequestLoginJson
+            {
+                Email = _email,
+                Password = _password
+            };
+
+            var response = await DoPost(method, request);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.OK);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            responseData.RootElement.GetProperty("name").GetString().ShouldSatisfyAllConditions(
+                name => name.ShouldNotBeNullOrWhiteSpace(),
+                name => name.ShouldBe(_name)
+                );
+            responseData.RootElement.GetProperty("tokens").GetProperty("accessToken").GetString().ShouldNotBeNullOrEmpty();
+        }
+
+        [Theory]
+        [ClassData(typeof(CultureInlineDataTests))]
+        public async Task Error_Login_Invalid(string culture)
+        {
+            var request = RequestLoginJsonBuilder.Build();
+
+            var response = await DoPost(method, request, culture);
+
+            response.StatusCode.ShouldBe(HttpStatusCode.Unauthorized);
+
+            await using var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var responseData = await JsonDocument.ParseAsync(responseBody);
+
+            var errors = responseData.RootElement.GetProperty("errors").EnumerateArray();
+
+            var expectedMessage = ResourceMessagesException.ResourceManager.GetString("EMAIL_OR_PASSWORD_INVALID", new CultureInfo(culture));
+
+            errors.ShouldSatisfyAllConditions(
+                errors => errors.ShouldHaveSingleItem(),
+                errors => errors.ShouldContain(
+                    error => error.GetString()!.Equals(expectedMessage)
+                )
+            );
+        }
+    }
+}
