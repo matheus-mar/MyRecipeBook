4524cb6a577e85f0fbdc4d6ae84946cbaec5f912
Matheus Rocha <matheus.alvesrocha@hotmail.com>
2025-11-22 21:29:58 -0300

    fix: improving urls to be lowercase

--- tests/WebApi.Test/User/Register/RegisterUserTest.cs
+++ tests/WebApi.Test/User/Register/RegisterUserTest.cs
@@ -10,18 +10,18 @@ using WebApi.Test.InlineData;
 
 namespace WebApi.Test.User.Register
 {
-    public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
+    public class RegisterUserTest : MyRecipeBookClassFixture
     {
-        private readonly HttpClient _httpClient;
+        private readonly string method = "user";
 
-        public RegisterUserTest(CustomWebApplicationFactory factory) => _httpClient = factory.CreateClient();
+        public RegisterUserTest(CustomWebApplicationFactory factory) : base(factory) { }
 
         [Fact]
         public async Task Success()
         {
             var request = RequestRegisterUserJsonBuilder.Build();
 
-            var response = await _httpClient.PostAsJsonAsync("User", request);
+            var response = await DoPost(method, request);
 
             response.StatusCode.ShouldBe(HttpStatusCode.Created);
             
@@ -33,6 +33,7 @@ namespace WebApi.Test.User.Register
                 name => name.ShouldNotBeNullOrWhiteSpace(),
                 name => name.ShouldBe(request.Name)
                 );
+            responseData.RootElement.GetProperty("tokens").GetProperty("accessToken").GetString().ShouldNotBeNullOrEmpty();
         }
 
         [Theory]
@@ -42,12 +43,7 @@ namespace WebApi.Test.User.Register
             var request = RequestRegisterUserJsonBuilder.Build();
             request.Name = string.Empty;
 
-            if (_httpClient.DefaultRequestHeaders.Contains("Accept-Language"))
-                _httpClient.DefaultRequestHeaders.Remove("Accept-Language");
-
-            _httpClient.DefaultRequestHeaders.Add("Accept-Language", culture);
-
-            var response = await _httpClient.PostAsJsonAsync("User", request);
+            var response = await DoPost(method, request, culture);
 
             response.StatusCode.ShouldBe(HttpStatusCode.BadRequest);
             
